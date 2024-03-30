798ec30932bd262ba0d355d840e983a14fa5f746
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-03-30 18:00:03 +0100

    Add react-transition-group library and implement CSS transitions

--- src/components/SearchPage/Mainlist/SearchLocalList.tsx
+++ src/components/SearchPage/Mainlist/SearchLocalList.tsx
@@ -3,6 +3,7 @@ import MobileFilterCategory from "./inner/MobileFilterCategory";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../redux/store/store";
 import { ListaRistorantiResponse } from "../../../interfaces/interfaces";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 function SearchLocalList() {
   const listaRistoranti = useSelector((state: RootState) => state.searchRistorante.listaRistoranti);
@@ -15,38 +16,41 @@ function SearchLocalList() {
   return (
     <div>
       <MobileFilterCategory />
-      <Row>
-        {filteredRestaurants?.map((ristorante: ListaRistorantiResponse, index) => (
-          <Col key={`ristorante-${index}`} className="col-xxl-3 col-xl-4 col-sm-6 col-12 px-2">
-            <div className="py-2 px-2 my-3 shadow ">
-              <a
-                href="https://take2me.it/locali/canapa-house-riccione.html"
-                className="d-block restaurant-img noimage bg-cover rounded-3 position-relative lazy"
-                style={{
-                  backgroundImage:
-                    'url("https://thumbnails.take2me.it/w_348/h_230/fit_crop-bottom/url/https://cdn.take2me.it/img/restaurants/e/9/f/e9f7ba27-32e4-4ad3-95f5-53515a3a3347.jpg")',
-                }}
-              >
-                <img
-                  src="https://thumbnails.take2me.it/w_50/h_50/fit_crop-bottom/url/https://cdn.take2me.it/img/restaurants/8/0/d/80d2e290-1176-4764-a65f-ac1de8e4f97f.jpg"
-                  className="img-logo border-lightw"
-                  alt="Canapa House "
-                />
-              </a>
-              <div className="pt-5">
-                <a href="https://take2me.it/locali/canapa-house-riccione.html" className="text-secondary">
-                  <h5 className="h5 mb-0 fw-bold ">{ristorante.nomeRistorante}</h5>
-                </a>
 
-                <Row className="justify-content-between">
-                  <Col className="fw-light">{ristorante.descrizione}</Col>
-                  <Col className=" text-end">{ristorante.distanza.toFixed(2)} km</Col>
-                </Row>
+      <TransitionGroup className="row">
+        {filteredRestaurants?.map((ristorante: ListaRistorantiResponse, index) => (
+          <CSSTransition key={`ristorante-${index}`} timeout={500} classNames="fade">
+            <Col className="col-xxl-3 col-xl-4 col-sm-6 col-12 px-2">
+              <div className="py-2 px-2 my-3 shadow ">
+                <a
+                  href="https://take2me.it/locali/canapa-house-riccione.html"
+                  className="d-block restaurant-img noimage bg-cover rounded-3 position-relative lazy"
+                  style={{
+                    backgroundImage:
+                      'url("https://thumbnails.take2me.it/w_348/h_230/fit_crop-bottom/url/https://cdn.take2me.it/img/restaurants/e/9/f/e9f7ba27-32e4-4ad3-95f5-53515a3a3347.jpg")',
+                  }}
+                >
+                  <img
+                    src="https://thumbnails.take2me.it/w_50/h_50/fit_crop-bottom/url/https://cdn.take2me.it/img/restaurants/8/0/d/80d2e290-1176-4764-a65f-ac1de8e4f97f.jpg"
+                    className="img-logo border-lightw"
+                    alt="Canapa House "
+                  />
+                </a>
+                <div className="pt-5">
+                  <a href="https://take2me.it/locali/canapa-house-riccione.html" className="text-secondary">
+                    <h5 className="h5 mb-0 fw-bold ">{ristorante.nomeRistorante}</h5>
+                  </a>
+
+                  <Row className="justify-content-between">
+                    <Col className="fw-light">{ristorante.descrizione}</Col>
+                    <Col className=" text-end">{ristorante.distanza.toFixed(2)} km</Col>
+                  </Row>
+                </div>
               </div>
-            </div>
-          </Col>
+            </Col>
+          </CSSTransition>
         ))}
-      </Row>
+      </TransitionGroup>
     </div>
   );
 }
