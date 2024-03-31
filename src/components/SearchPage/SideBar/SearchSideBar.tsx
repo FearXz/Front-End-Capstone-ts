76d8907ba15a2e0cf02da6145b1ba58eec3610f4
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-03-31 21:53:27 +0200

    Add min-height to main element and add z-index to search sidebar

--- src/components/SearchPage/SideBar/SearchSideBar.tsx
+++ src/components/SearchPage/SideBar/SearchSideBar.tsx
@@ -3,7 +3,7 @@ import SearchFormLocal from "./inner/SearchFormLocal";
 
 function SearchSideBar() {
   return (
-    <div id="search-filters" className="bg-white px-3 sticky-top">
+    <div id="search-filters" className="bg-white px-3 sticky-top z-1">
       <SearchFormLocal />
       <CategoryFilter />
     </div>
